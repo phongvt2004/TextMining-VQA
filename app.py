ca427803a8da1843af873d45ec1c91286d3b5b1d
phonghocode <ntp11892vt@gmail.com>
2025-05-29 18:02:00 +0700

    refactor

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 page1 = st.Page("check.py", title='Check Test Data')
 page2 = st.Page("models.py", title='Models')
-page3 = st.Page("models_2.py", title='Models_2')
 # pg = st.navigation([page1, page2])
 pg = st.navigation([page2])
 
